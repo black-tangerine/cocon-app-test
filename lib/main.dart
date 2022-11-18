6ef9a15de5256d16b8a41d1ba71931ce4d7e68f4
JiSoo Park <rscracker@naver.com>
2022-11-18 12:25:25 +0900

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       title: 'COCON APP',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
       ),
       initialBinding: BindingsBuilder(() => {
         Get.put(MainController())
